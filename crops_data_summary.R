0e5d0655d056dc78fe0b56e4da2ec1bb60a30f77
Abigail.Feuka <abigail.feuka@usda.gov>
2025-01-21 16:05:51 -0700

    adding spatial model folder

--- crops_data_summary.R
+++ crops_data_summary.R
@@ -4,10 +4,10 @@
 
 library(tidyverse)
 
-source("./crop_planting_anom/Functions/clean_crop_dat.R")
+source("./Functions/clean_crop_dat.R")
 
 #all counties-------------
-load("./Data/all_crops_anom_scaled_2023_2009_anom.RData")
+load("C:/Users/Abigail.Feuka/OneDrive - USDA/Feral Hogs/Crops/Data/all_crops_anom_scaled_2023_2009_anom.RData")
 dat_orig <- dat
 
 commod_names_c <- unique(dat_orig$commodity_desc)
@@ -19,18 +19,27 @@ counties_df <- data.frame(Crop=commod_names_t,
                          nCounties=NA,
                          nEcoRegions=NA,
                          n=NA)
+
+nb=FALSE
+temporal=TRUE
+
 for(commod_idx in 1:length(commod_names_c)){
-  dat_op  <- clean_crop_dat(dat_orig=dat_orig,commod_name = commod_names[commod_idx], only_pigs = T)
+  dat_op  <- clean_crop_dat(dat_orig=dat_orig,
+                            nb=nb,
+                            temporal=temporal,
+                            commod_name = commod_names[commod_idx], only_pigs = T)
   
   dat_clean_op <- dat_op$dat_clean
-  covsx_op  <- dat_op$covsx
-  xmat <- dat_op$xmat
-  reg_count_idx <- dat_op$reg_count_idx
+  # covsx_op  <- dat_op$covsx
+  # xmat <- dat_op$xmat
+  # reg_count_idx <- dat_op$reg_count_idx
   
-  if("take.hog.intens.5yeartrend.sc"%in%colnames(dat_clean_op)){
+  if("take.hog.intens.5yeartrend.sc"%in%colnames(dat_clean) & mod_typ!="spatial"){
     subfolder <- "Take Trend"
-  } else if("take.hog.intens.prev.sc"%in%colnames(dat_clean_op)) {
+  } else if("take.hog.intens.prev.sc"%in%colnames(dat_clean) & mod_typ!="spatial") {
     subfolder <- "Take Previous" 
+  } else if(mod_typ=="spatial") {
+    subfolder <- "Spatial" 
   } else {
     stop("Must include take covariate. Check clean_crop_dat.R")
   }
@@ -39,4 +48,5 @@ for(commod_idx in 1:length(commod_names_c)){
   counties_df$nEcoRegions[commod_idx] <- length(unique(dat_clean_op$division_grp))
   counties_df$n[commod_idx] <-  nrow(dat_clean_op)
 }
+
 write.csv(counties_df,paste0("C:/Users/Abigail.Feuka/OneDrive - USDA/Feral Hogs/Crops/Model outputs/",subfolder,"/Plots/Combined Figures/data_summary_table.csv"))
