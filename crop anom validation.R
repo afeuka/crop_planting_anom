693fb6fece0afd969691b69d88941a0775035a84
Abigail.Feuka <abigail.feuka@usda.gov>
2025-01-21 16:05:01 -0700

    adding r2 calculations to validation

--- crop anom validation.R
+++ crop anom validation.R
@@ -23,7 +23,8 @@ commod_names_t <- str_to_title(commod_names_c)
 commod_names <- tolower(commod_names_c)
 
 commod_idx <- 1
-pval <- numeric()
+pval <- r2_v <- samp_mn<-samp_sd<- numeric()
+ypred_sum_list <- ypred_stat_list<-list()
 for(commod_idx in 1:length(commod_names_c)){
   # all counties-------------
   dat <- clean_crop_dat(dat_orig=dat_orig,
@@ -140,6 +141,8 @@ for(commod_idx in 1:length(commod_names_c)){
                             commod_name = commod_names[commod_idx], only_pigs = T)
   
   dat_clean_op <- dat_op$dat_clean
+  samp_mn[commod_idx] <- mean(dat_clean_op$plant.anom)
+  samp_sd[commod_idx] <- sqrt(var(dat_clean_op$plant.anom))
   
   ##model checks ---------------------
   ypred <- samples_all[,grep("ypred",colnames(samples_all))]
@@ -176,36 +179,24 @@ for(commod_idx in 1:length(commod_names_c)){
   pval[commod_idx] <- sum(dat_ll>pred_ll)/nrow(mu)
   
   ###posterior predictive distribution ----------------
-  ggplot(ypred_sum %>% pivot_longer(cols=c("mn","obs"),
-                                    values_to="value",
-                                    names_to="typ"))+
-    geom_histogram(aes(x=value,fill=typ),alpha=0.5,col="transparent")+
-    scale_fill_discrete(name="",labels=c("Post pred",
-                                         "Data"))+
-    ggtitle(paste(commod_names_t[commod_idx]," - Posterior predictions - Counties with pigs"))
-  
-  if(!dir.exists(paste0("./Model outputs/",subfolder,"/Plots/",commod_names_t[commod_idx]))){
-    dir.create(paste0("./Model outputs/",subfolder,"/Plots/",commod_names_t[commod_idx]))
-  }
-  ggsave(filename=paste0("./Model outputs/",subfolder,"/Plots/",commod_names_t[commod_idx],"/data_post_dist_op.jpeg"),
-         device="jpeg",height=5,width=7,units="in")
+  ypred_sum <- ypred_sum %>% pivot_longer(cols=c("mn","obs"),
+                             values_to="value",
+                             names_to="typ")
+  ypred_sum$crop <- commod_names[commod_idx]
+  ypred_sum_list[[commod_idx]] <- ypred_sum
   
   ##simulated model stats --------------------
   ypred_stat <- data.frame(mn=rowMeans(ypred),
                            sd=apply(ypred,1,sd),
                            idx=1:nrow(ypred))
-  ggplot()+
-    geom_histogram(data=ypred_stat,aes(x=mn))+
-    geom_vline(xintercept=mean(dat_clean_op$plant.anom),col="red",lty=2)+
-    ggtitle(paste(commod_names_t[commod_idx]," - Posterior data mean - Counties with pigs"))
-  ggsave(filename=paste0("./Model outputs/",subfolder,"/Plots/",commod_names_t[commod_idx],"/pval_mn_op.jpeg"),
-         device="jpeg",width=7,height=5,units="in")
-  ggplot()+
-    geom_histogram(data=ypred_stat,aes(x=sd))+
-    geom_vline(xintercept=sqrt(var(dat_clean_op$plant.anom)),col="red",lty=2)+
-    ggtitle(paste(commod_names_t[commod_idx]," - Posterior data SD - Counties with pigs"))
-  ggsave(filename=paste0("./Model outputs/",subfolder,"/Plots/",commod_names_t[commod_idx],"/pval_sd_op.jpeg"),
-         device="jpeg",width=7,height=5,units="in")
+  
+  ypred_stat$crop <- commod_names[commod_idx]
+  ypred_stat_list[[commod_idx]] <- ypred_stat
+  
+  #r2 ------------
+  ssr <- sum(ypred_sum$resid^2)
+  sst <- sum((ypred_sum$plant.anom-mean(ypred_sum$plant.anom))^2)
+  r2_v[commod_idx] <- 1- ssr/sst
   
   # ### standardized residuals--------------------
   # ggplot(ypred_sum)+
@@ -241,11 +232,48 @@ for(commod_idx in 1:length(commod_names_c)){
   
 }
 pval_df <- data.frame(Crop=commod_names_t,pval)
+r2_df <- data.frame(Crop=commod_names_t,r2_v)
+fit_df <- pval_df %>% left_join(r2_df)
 
 if(!dir.exists(paste0("C:/Users/Abigail.Feuka/OneDrive - USDA/Feral Hogs/Crops/Model outputs/",
                       subfolder,"/Plots/Combined Figures"))){
   dir.create(paste0("C:/Users/Abigail.Feuka/OneDrive - USDA/Feral Hogs/Crops/Model outputs/",
                          subfolder,"/Plots/Combined Figures"))
 }
-write.csv(pval_df,paste0("C:/Users/Abigail.Feuka/OneDrive - USDA/Feral Hogs/Crops/Model outputs/",
-                         subfolder,"/Plots/Combined Figures/pval_table.csv"))
+write.csv(fit_df,paste0("C:/Users/Abigail.Feuka/OneDrive - USDA/Feral Hogs/Crops/Model outputs/",
+                         subfolder,"/Plots/Combined Figures/pval_r2_table.csv"))
+
+#posterior predictive vs value 
+ypred_stat <- do.call("rbind.data.frame",ypred_stat_list)
+samp_mn <- data.frame(mn=samp_mn,crop=commod_names)
+samp_sd <- data.frame(sd=samp_sd,crop=commod_names)
+ggplot()+
+  geom_histogram(data=ypred_stat,aes(x=mn))+
+  geom_vline(data=samp_mn,aes(xintercept=mn),col="red",lty=2)+
+  facet_wrap(.~crop)+
+  ggtitle("Posterior data mean - Counties with pigs")
+ggsave(filename=paste0("C:/Users/Abigail.Feuka/OneDrive - USDA/Feral Hogs/Crops/Model outputs/",
+                       subfolder,"/Plots/Combined Figures/pval_mn.jpeg"),
+       device="jpeg",width=7,height=5,units="in")
+ggplot()+
+  geom_histogram(data=ypred_stat,aes(x=sd))+
+  geom_vline(data=samp_sd,aes(xintercept=sd),col="red",lty=2)+
+  facet_wrap(.~crop)+
+  ggtitle("Posterior data SD - Counties with pigs")
+ggsave(filename=paste0("C:/Users/Abigail.Feuka/OneDrive - USDA/Feral Hogs/Crops/Model outputs/",
+                       subfolder,"/Plots/Combined Figures/pval_sd.jpeg"),
+       device="jpeg",width=7,height=5,units="in")
+
+
+#posterior predictive vs data
+ypred_sum <- do.call("rbind.data.frame",ypred_sum_list)
+ggplot(ypred_sum)+
+  geom_histogram(aes(x=value,fill=typ),alpha=0.5,col="transparent")+
+  facet_wrap(.~crop)+
+  scale_fill_discrete(name="",labels=c("Post pred",
+                                       "Data"))+
+  ggtitle("Posterior predictions - Counties with pigs")
+
+ggsave(filename=paste0("C:/Users/Abigail.Feuka/OneDrive - USDA/Feral Hogs/Crops/Model outputs/",
+                       subfolder,"/Plots/Combined Figures/data_post_dist_op.jpeg"),
+       device="jpeg",height=5,width=7,units="in")
